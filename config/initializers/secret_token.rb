ef7a1fd939a62db4311285d6499fd13a84d50a96
john kim <johnkim@johns-MacBook-Air.local>
2013-08-05 06:52:09 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialNetwork2::Application.config.secret_key_base = 'ddceda2be0f51895332e2be8dbc749d35309a826aec2847d0e0951ef529feb751f9834fd983e9672ea51bc34337c74cb363b5b31c91c5be8f1f883a9a4f6c1d5'
